f8f84ca24d6e74f5b914e1d3ed4381be9d31f5d8
Daniel Salazar <daniel.esalazars@gmail.com>
2017-09-03 15:18:57 -0500

    Menú offCanvas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 $(document).foundation()
 
 $("[data-toggle-dia]").click(function(event) {
-	const panel = $(this).data("toggleDia");
-	$("#lineup-tabs").foundation('selectTab',panel);
+	const panel = $(this).data("toggleDia")
+	$("#lineup-tabs").foundation('selectTab', panel)
+})
+
+const $offCanvas = $("#offCanvas")
+
+$offCanvas.find('li').click(function(event) {
+	$offCanvas.foundation("close");
 })
